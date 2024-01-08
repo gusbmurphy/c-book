c85460e5ea4cac99522277a09c8f6fe2a97d8d01
Augustus Murphy <gusbmurphy@gmail.com>
2024-01-07 21:31:47 -0600

    Finish my_strcat

--- strcat.c
+++ strcat.c
@@ -10,9 +10,27 @@ int main()
 
   my_strcat(str_a, str_b);
 
+  puts(str_a);
+  putchar('\n');
+
+  puts(str_b);
+  putchar('\n');
+
   puts(strcmp(str_a, "Hello, world!") == 0 ? "PASS" : "FAIL");
 }
 
 void my_strcat(char *a, char *b) {
-  return;
+  int i = 0;
+
+  // Find the end of "a"...
+  while (a[i] != '\0') {
+    i++;
+  }
+
+  // ...then start adding "b" from there.
+  for (int j = 0; b[j] != '\0'; i++, j++) {
+    a[i] = b[j];
+  }
+
+  a[i] = '\0';
 }
